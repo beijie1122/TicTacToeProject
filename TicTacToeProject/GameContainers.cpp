f7dcd72bdb0c42572724aa86cf8eb2292c0d490b
beijie1122 <111162504+beijie1122@users.noreply.github.com>
2023-04-30 22:42:47 -0400

    Completed 1P Quadrant Override

--- TicTacToeProject/GameContainers.cpp
+++ TicTacToeProject/GameContainers.cpp
@@ -63,24 +63,151 @@ void GameContainers::MainGameMenu()
 
 		if (SelectionVariable == 1)
 		{
-			Q1Test();
+			Q1Selection();
+		}
+		else if (SelectionVariable == 2)
+		{
+			Q2Selection();
+		}
+		else if (SelectionVariable == 3)
+		{
+			Q3Selection();
+		}
+		else if (SelectionVariable == 4)
+		{
+			Q4Selection();
+		}
+		else if (SelectionVariable == 5)
+		{
+			Q5Selection();
+		}
+		else if (SelectionVariable == 6)
+		{
+			Q6Selection();
+		}
+		else if (SelectionVariable == 7)
+		{
+			Q7Selection();
+		}
+		else if (SelectionVariable == 8)
+		{
+			Q8Selection();
+		}
+		else if (SelectionVariable == 9)
+		{
+			Q9Selection();
 		}
 
 
-	} while (SelectionVariable != 9);
+	} while (SelectionVariable != 0);
 }
 
-void GameContainers::Q1Test()
+void GameContainers::Q1Selection()
 {
-	size_t QuadrantMinXValue = 1;
-	size_t QuadrantMinYValue = 1;
+	size_t QuadrantMinXValue = 0;
+	size_t QuadrantMinYValue = 0;
 	size_t QuadrantMaxXValue = 3;
 	size_t QuadrantMaxYValue = 3;
-	char Player1Symbol = 'X';
 
-	for (size_t i = 0; i < QuadrantMaxYValue; i++)
+	//Legacy for Testing Quadrant Override
+	/*
+	for (size_t i = QuadrantMinYValue; i < QuadrantMaxYValue; i++)
 	{
-		for (size_t j = 0; j < QuadrantMaxXValue; j++)
+		for (size_t j = QuadrantMinXValue; j < QuadrantMaxXValue; j++)
+		{
+			BaseBoard[i][j] = Player1Symbol;
+		}
+	}
+	*/
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q2Selection()
+{
+	size_t QuadrantMinXValue = 4;
+	size_t QuadrantMinYValue = 0;
+	size_t QuadrantMaxXValue = 7;
+	size_t QuadrantMaxYValue = 3;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q3Selection()
+{
+	size_t QuadrantMinXValue = 8;
+	size_t QuadrantMinYValue = 0;
+	size_t QuadrantMaxXValue = 11;
+	size_t QuadrantMaxYValue = 3;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q4Selection()
+{
+	size_t QuadrantMinXValue = 0;
+	size_t QuadrantMinYValue = 4;
+	size_t QuadrantMaxXValue = 3;
+	size_t QuadrantMaxYValue = 7;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q5Selection()
+{
+	size_t QuadrantMinXValue = 4;
+	size_t QuadrantMinYValue = 4;
+	size_t QuadrantMaxXValue = 7;
+	size_t QuadrantMaxYValue = 7;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q6Selection()
+{
+	size_t QuadrantMinXValue = 8;
+	size_t QuadrantMinYValue = 4;
+	size_t QuadrantMaxXValue = 11;
+	size_t QuadrantMaxYValue = 7;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q7Selection()
+{
+	size_t QuadrantMinXValue = 0;
+	size_t QuadrantMinYValue = 8;
+	size_t QuadrantMaxXValue = 3;
+	size_t QuadrantMaxYValue = 11;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q8Selection()
+{
+	size_t QuadrantMinXValue = 4;
+	size_t QuadrantMinYValue = 8;
+	size_t QuadrantMaxXValue = 7;
+	size_t QuadrantMaxYValue = 11;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::Q9Selection()
+{
+	size_t QuadrantMinXValue = 8;
+	size_t QuadrantMinYValue = 8;
+	size_t QuadrantMaxXValue = 11;
+	size_t QuadrantMaxYValue = 11;
+
+	QuadrantOverride(QuadrantMinXValue, QuadrantMinYValue, QuadrantMaxXValue, QuadrantMaxYValue);
+}
+
+void GameContainers::QuadrantOverride(size_t& MinX, size_t& MinY, size_t& MaxX, size_t& MaxY)
+{
+	for (size_t i = MinY; i < MaxY; i++)
+	{
+		for (size_t j = MinX; j < MaxX; j++)
 		{
 			BaseBoard[i][j] = Player1Symbol;
 		}
--- TicTacToeProject/GameContainers.h
+++ TicTacToeProject/GameContainers.h
@@ -15,6 +15,12 @@ public:
 
 	std::vector<std::vector<char>> BaseBoard{ VectorColumn, std::vector<char>(VectorRow) };
 
+	size_t QuadrantMinXValue;
+	size_t QuadrantMinYValue;
+	size_t QuadrantMaxXValue;
+	size_t QuadrantMaxYValue;
+	char Player1Symbol = 'X';
+
 
 	GameContainers();
 
@@ -26,7 +32,26 @@ public:
 
 	void MainGameMenu();
 
-	void Q1Test();
+	void Q1Selection();
+
+	void Q2Selection();
+
+	void Q3Selection();
+
+	void Q4Selection();
+
+	void Q5Selection();
+
+	void Q6Selection();
+
+	void Q7Selection();
+
+	void Q8Selection();
+
+	void Q9Selection();
+
+
+	void QuadrantOverride(size_t &MinX, size_t &MinY, size_t &MaxX, size_t &MaxY);
 
 
 	~GameContainers();
